2e2386929429635067d566316dfabd3b34775593
Muhammad Conn <muhammad.conn@icloud.com>
2025-03-23 21:58:25 -0700

    Developed the requested arrays and methods for displaying co2 data

--- Main.java
+++ Main.java
@@ -33,6 +33,36 @@ class Main
   public static void main(String[] args) 
   {
 		// DECLARATION SECTION
+    double[] co2Levels = new double[20];
+    co2Levels[0] = 371.32;
+    co2Levels[1] = 373.45;
+    co2Levels[2] = 375.98;
+    co2Levels[3] = 377.70;
+    co2Levels[4] = 379.98;
+    co2Levels[5] = 382.09;
+    co2Levels[6] = 384.03;
+    co2Levels[7] = 385.83;
+    co2Levels[8] = 387.64;
+    co2Levels[9] = 390.10;
+    co2Levels[10] = 391.85;
+    co2Levels[11] = 394.06;
+    co2Levels[12] = 396.74;
+    co2Levels[13] = 398.87;
+    co2Levels[14] = 401.01;
+    co2Levels[15] = 404.41;
+    co2Levels[16] = 406.76;
+    co2Levels[17] = 408.72;
+    co2Levels[18] = 411.66;
+    co2Levels[19] = 414.24;
+
+    int numYears;
+    int[] years = new int[20];
+    for (numYears = 0; numYears < 20; numYears++)
+    {
+      years[numYears] = 2001 + numYears;
+    }
+
+
 
 		// INITIALIZATION SECTION
 
@@ -41,14 +71,39 @@ class Main
 		// PROCESSING SECTION
 
 		// OUTPUT SECTION
-  
+   
     //Explanation of oil drum units
     System.out.println("\nData displayed for each year begins at 360 ppm.\nEach additional oil drum (🛢 ) represents an additional 10 ppm.\n");
-
+    
     //Column titles of graph
     System.out.print("Year");
     UtilityBelt.printCentered(55,"CO₂ in Atmosphere (ppm)");
 
+    printGraph(years,co2Levels);
+    System.out.printf("%s%.2f%s", "From 2001 to 2020, the average atmospheric CO₂ levels across the globe has grown ", (co2Levels[co2Levels.length - 1] - co2Levels[0]), " ppm.");
+
+
+
+  }
+  // STATIC METHODS SECTION
+  public static void printBar(double co2Level)
+  {
+    int numOilDrums;
+    for (numOilDrums = 0; numOilDrums < (int)(co2Level - 360); numOilDrums++)
+    {
+      System.out.print("🛢");
+    }
+    System.out.println(" " + co2Level);
   }
 
+  public static void printGraph(int[] year, double[] co2Level)
+  {
+    int loopYear;
+    for (loopYear = 0; loopYear < 20; loopYear++)
+    {
+      System.out.print(year[loopYear] + " ");
+      printBar(co2Level[loopYear]);
+    }
+
+  }
 }
